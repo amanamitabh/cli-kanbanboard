f0eda61f3c0cc5c52d37392c344011e6f2c1b3cd
Aman Amitabh <amanamitabh@gmail.com>
2024-04-09 19:09:35 +0530

    Established Connection with DB

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+import os
+from dotenv import load_dotenv
+import mysql.connector as mysql
+
+load_dotenv()
+DB_HOSTNAME = os.getenv('DB_HOST')
+DB_USERNAME = os.getenv('DB_USER')
+DB_PASSWD = os.getenv('DB_PASSWORD')
+DB_DATABASENAME = os.getenv('DB_DBNAME')
+
+con_obj = mysql.connect(host=DB_HOSTNAME, user=DB_USERNAME, password=DB_PASSWD, database=DB_DATABASENAME)
+cur_obj = con_obj.cursor()
+print("Connection Established")
+'''
 class Task:
-    def __init__(self, taskname, category, status, assignees, reporters):
+    def __init__(self, taskname, category, status, priority, assignees, reporters):
         self.__taskname = taskname
         self.__category = category
         self.__status = status
+        self.priority = priority
         self.__assignees = assignees
         self.__reporters = reporters
-    
-    def getData(self):
-        print(f"{self.__taskname}, {self.__category}, {self.__status}, {self.__assignees}, {self.__reporters}")
 
-t1 = Task("Stop Aryan from Watching TV", "Marketing", "To-Do", ["Aditya N Menon"], ["Aman Amitabh"])
-t1.getData()
+def getData(self):
+        print(f"{self.__taskname}, {self.__category}, {self.__status}, {self.__assignees}, {self.__reporters}")
+'''
+
+con_obj.close()
+print("Connection Closed")
