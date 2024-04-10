4f07efeb6737aaf7b8b39339a1f7d8cd2fce8dda
Aman Amitabh <amanamitabh@gmail.com>
2024-04-11 00:04:01 +0530

    Added Functions to fetch task data, add task and remove task

--- main.py
+++ main.py
@@ -11,19 +11,45 @@ DB_DATABASENAME = os.getenv('DB_DBNAME')
 con_obj = mysql.connect(host=DB_HOSTNAME, user=DB_USERNAME, password=DB_PASSWD, database=DB_DATABASENAME)
 cur_obj = con_obj.cursor()
 print("Connection Established")
-'''
-class Task:
-    def __init__(self, taskname, category, status, priority, assignees, reporters):
-        self.__taskname = taskname
-        self.__category = category
-        self.__status = status
-        self.priority = priority
-        self.__assignees = assignees
-        self.__reporters = reporters
 
-def getData(self):
-        print(f"{self.__taskname}, {self.__category}, {self.__status}, {self.__assignees}, {self.__reporters}")
-'''
+def getKanbanBoard():
+    qry = "SELECT * FROM tasks;"
+    cur_obj.execute(qry)
+    data = cur_obj.fetchall()
+    return data
+
+
+def getTask(taskname):
+    qry = "SELECT * FROM tasks WHERE taskname = %s;"
+    cur_obj.execute(qry, [taskname])
+    data = cur_obj.fetchall()
+    return data
+
+
+def addTask(taskname, category, status, priority, assignees, reportees):
+    qry = "INSERT INTO tasks VALUES(%s, %s, %s, %s, %s, %s);"
+    vals = [taskname, category, status, priority, assignees, reportees]
+    cur_obj.execute(qry, vals)
+    con_obj.commit()
+
+
+def removeTask(taskname):
+    rem_data = getTask(taskname)
+    print("Are you sure this is the task you want to remove : ")
+    for i in rem_data:
+        print(i)
+
+    confirm = input("Enter 'Y' if you want to proceed : ")
+    if confirm == "Y":
+        vals = [taskname]
+        qry = "DELETE FROM tasks WHERE taskname = %s;"
+        cur_obj.execute(qry, vals)
+        con_obj.commit()
+
+
+table_data = getKanbanBoard()
+for i in table_data:
+    print(i)
 
 con_obj.close()
 print("Connection Closed")
